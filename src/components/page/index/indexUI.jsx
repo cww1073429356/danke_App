eb9ad56aa1ff6277718456f0957aa0a145c3b1b7
qq110120119 <1975564168@qq.com>
2018-11-29 16:30:49 +0800

    '样式'

--- src/components/page/index/indexUI.jsx
+++ src/components/page/index/indexUI.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import logo from '../images/logo2.png'
 
 export default function(props){
     return(
-        <div id="index"  className="page">
-            <div >
-            <h1>111111</h1>
+        <div id="index" className="page">
+            <div  className="title">
+                <h1>
+                    <img src={logo} alt=""/>
+                </h1>
+                <div className="dkcity">
+                    <button type="button">深圳</button>
+                </div>
             </div>
-            <div>
-                <input/>
-                <p>查询</p>
-            </div>
+           
         </div>
     )
 }
